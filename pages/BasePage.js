77096a5717644b610621c5f244920d09db88d44c
kimys93 <kimys@ngle.co.kr>
2025-11-24 18:03:01 +0900

    BasePage.js baseURL ?щ옒??以묐났 臾몄젣 ?섏젙
    - baseURL ?앹쓽 ?щ옒???쒓굅: http://10.10.0.159:8000/ -> http://10.10.0.159:8000
    - goto 硫붿꽌??媛꾩냼??- URI ?щ옒??以묐났 臾몄젣 ?닿껐

--- pages/BasePage.js
+++ pages/BasePage.js
@@ -11,7 +11,7 @@ dotenv.config();
 class BasePage {
   constructor(page) {
     this.page = page;
-    this.baseURL = 'http://10.10.0.159:8000/';
+    this.baseURL = 'http://10.10.0.159:8000';
     // 기본 요소들
     this.body = this.page.locator('body');
     this.navigation = this.page.locator('nav, header, .navbar');
