13d8c06f5fd713a245c670503cb88b33573653bd
kimys93 <kimys@ngle.co.kr>
2025-11-24 13:18:37 +0900

    fix: Grafana Failed Test Error Logs 荑쇰━ ?섏젙 諛?????ㅻ쪟 ?섏젙

--- tests-sanity/sanity.spec.js
+++ tests-sanity/sanity.spec.js
@@ -1,5 +1,4 @@
 // @ts-check
-// @ts-ignore - @playwright/test 타입 선언이 자동으로 로드됨
 import { test, expect } from '@playwright/test';
 import BasePage from '../pages/BasePage.js';
 import LoginPage from '../pages/LoginPage.js';
